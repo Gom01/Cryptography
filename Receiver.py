a306d4bb7137275409f626d2989d491e037977c7
Gom01 <flgomez@bluewin.ch>
2024-02-28 16:40:37 +0100

    Comments and names

--- Receiver.py
+++ Receiver.py
@@ -4,15 +4,16 @@ import socket
 IP = "vlbelintrocrypto.hevs.ch"
 PORT = 6000
 
+# Decode the message
 def decode_message(message):
     type = chr(message[3])
     res = ''
 
     if type == 't':
-        message = message [6:]
+        message = message [6:] #remove 6first
         for i in range(len(message)):
             if (i + 1) % 4 == 0:
-                res += message[i].to_bytes(1, byteorder='big').decode("utf-8")
+                res += message[i].to_bytes(1, byteorder='big').decode("utf-8") #Why convert to byte and convert back in Chr
         
     return res
         
@@ -21,7 +22,8 @@ def decode_message(message):
 client_socket = socket.socket()
 client_socket.connect((IP,PORT))
 
+#Listening the server responses
 while True:
-    data = client_socket.recv(1024) # receive response
+    data = client_socket.recv() # receive response
     message = decode_message(data)
     print(message)
--- Sender.py
+++ Sender.py
@@ -4,24 +4,27 @@ import socket
 IP = "vlbelintrocrypto.hevs.ch"
 PORT = 6000
 
+
+
 def create_message(message, type):
+    #Create a text message
     if type == 't':
-        message_length = len(message)
-        header = bytes(f"ISCt", 'utf-8')
-        length = message_length.to_bytes(2, byteorder='big')
-        res = header + length
+        message_length : int = len(message)
+        ISCt_header = bytes(f"ISCt", 'utf-8')
+        total_length = message_length.to_bytes(2, byteorder='big')
+        res = ISCt_header + total_length
         
         for i in range(message_length):
             res += bytes([0,0,0]) + bytes(str(message[i]), 'utf-8')
         
     return res
         
-
-
+# Creation of a socket and connection to the server
 client_socket = socket.socket()
 client_socket.connect((IP,PORT))
 
-message = create_message("Bonjour, test 1212", 't')
+
+message = create_message("Testing the message system.", 't')
 client_socket.send(message)  # send message
-data = client_socket.recv(1024) # receive response
+data = client_socket.recv() # receive response
 print(data)
