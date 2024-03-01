28f2082430c56a2a25005df3a1e3e58400b372de
Gom01 <flgomez@bluewin.ch>
2024-03-01 11:19:54 +0100

    Receiver is working

--- Receiver.py
+++ Receiver.py
@@ -13,13 +13,24 @@ def message_to_array(message):
 
     if type == 't':
         message = message [6:] #remove 6first
-        print(message[0:4])
         for i in range(0, len(message), 4):
             byte_val = message[i:i+4]
             res.append(int.from_bytes(byte_val, "big"))
         
     return res
         
+
+def array_to_message(array) :
+    new_Array = []
+    for i in array :
+        new_Array.append(i.to_bytes((i.bit_length()+7) // 8, 'big'))
+    res = ""
+    for i in new_Array :
+        res += i.decode('utf-8')
+
+    return(res)
+
+
 
 
 client_socket = socket.socket()
@@ -28,5 +39,7 @@ client_socket.connect((IP,PORT))
 #Listening the server responses
 while True:
     data = client_socket.recv(1024) # receive response
+    print(data)
     message = unshift(message_to_array(data), 1000)
-    print(message)
+    decode_message = (array_to_message(message))
+    print(decode_message)
