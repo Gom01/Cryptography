4af76d98707a4765b5e0c4c4b803304255f82197
Gom01 <flgomez@bluewin.ch>
2024-03-08 11:17:27 +0100

    Sender corrected

--- Sender.py
+++ Sender.py
@@ -1,52 +1,71 @@
-from Encode import *
-from tester import sendTestEncoding
-from Receiver import array_to_message, message_to_array2
+import sys
 import socket
+from Encode import *
 
-# Connection to the server
-IP = "vlbelintrocrypto.hevs.ch"
-PORT = 6000
+if __name__ == "__main__":
+    def create_msg(message, type, encoding):
+        # Transform message to table of int
+        array = msg_to_array(message)
+        # With this table => encoding
+        if encoding == "nothing":
+            print("You did nothing")
+        elif encoding == "shift":
+            value = int(input("Enter the shift value : "))
+            array = shift(array,value)
+
+        elif encoding == "xor":
+            value = int(input("Enter the number : "))
+            array = xor(array, value)
+
+        elif encoding == "vigenere":
+            keyword = str(input("Enter the keyword : "))
+            array = vigenere(array, keyword)
+
+        else:
+            print(f"The type : {type} doesn't exist")
+            sys.exit()
+
+        # Convert the table to bytes
+        final_message = Conversion_ArrayToByte(array, type)
+
+        # Proceed to connect socket and send the message
+        # Creation of a socket and connection to the server
+        client_socket = socket.socket()
+        client_socket.connect((IP, PORT))
+        client_socket.send(final_message)
 
 
-def create_message(arr_int, type):
-    # Create a text message
-    if type == 't' or type == 's':
-        message_length: int = len(arr_int)
-        ISCt_header = bytes(f"ISC{type}", 'utf-8')
-        total_length = message_length.to_bytes(2, byteorder='big')
-        res = ISCt_header + total_length
+    # This function is used to convert the Array[Int]to bytes
+    def Conversion_ArrayToByte(arr_int, type):
+        # Create a text message
+        if type == 't' or type == 's':
+            message_length: int = len(arr_int)
+            ISCt_header = bytes(f"ISC{type}", 'utf-8')
+            total_length = message_length.to_bytes(2, byteorder='big')
+            res = ISCt_header + total_length
+            for i in arr_int:
+                res += i.to_bytes(4, byteorder='big')
+        else:
+            print(f"The type {type} doesn't exist")
+            sys.exit()
 
-        for i in arr_int:
-            res += i.to_bytes(4, byteorder='big')
-    return res
-
-
-def message_to_array(message):
-    byte_arr = []
-    res = []
-
-    # string with encoding 'utf-8'
-    for c in message:
-        byte_arr.append(bytes(c, 'utf-8'))
-
-    for b in byte_arr:
-        res.append(int.from_bytes(b, "big"))
-
-    return res
+        return res
 
 
-# Creation of a socket and connection to the server
-client_socket = socket.socket()
-client_socket.connect((IP, PORT))
+    # Convert the given message to an array[Int] so we can encode it
+    def msg_to_array(message):
+        byte_arr = []
+        res = []
+        # string with encoding 'utf-8'
+        for c in message:
+            byte_arr.append(bytes(c, 'utf-8'))
+        for b in byte_arr:
+            res.append(int.from_bytes(b, "big"))
+        return res
 
-message = "Salut mon ami"
 
-encoded_message = vigenere(message_to_array(message), "Hello")
 
-client_socket.send(sendTestEncoding("vigenere", 4))  # send message
-server_m = array_to_message(message_to_array2(client_socket.recv(1024))).split()[-1]
-received_m = client_socket.recv(1024)
-print(vigenere(message_to_array2(received_m), server_m))
-encoded_m = array_to_message(vigenere(message_to_array2(received_m), server_m))
-client_socket.send(create_message(message_to_array(encoded_m), 's'))
-print(array_to_message(message_to_array2(client_socket.recv(1024))))
+    # Connection to the server
+    IP = "vlbelintrocrypto.hevs.ch"
+    PORT = 6000
+    create_msg("Salut mon ami, j'espère que tu vas bien !",'t',"xor")
