24e53d4f76e202b9974252f59d81675e6ac8a06f
Gom01 <flgomez@bluewin.ch>
2024-04-16 17:51:02 +0200

    diffieHellman completed

--- src/encode.py
+++ src/encode.py
@@ -1,44 +1,37 @@
 import math
 import random
-
 import numpy as np
 import time
-
 from .utils import loadData
 
-
+#Saved primes numbers
 array_prime_numbers = loadData("PrimeNumbersNumpy")
 array_prime_numbers5000 = loadData("PrimeNumbersNumpy5000")
 
 #Shift Encoding : Add Int to a table of Int
 def shift(array, shift_value):
     new_Array = []
-
     for i in range(len(array)):
         new_Array.append(array[i] + shift_value)
-    
     return new_Array
-
 
 #Xor Encoding : Xor two Int
 def xor(array, xor_value):
     new_Array = []
-
     for i in range(len(array)):
         new_Array.append(array[i]^xor_value)
-
     return new_Array
 
-
+#Vigenere encoding (need a keyword)
 def vigenere(array, keyword) :
     #Convert keyword to table[Int]
     new_Array = []
     keyArray = []
     for c in keyword :
         keyArray.append(ord(c))
-
     j = 0
     i = 0
+    #Add value of the letter to the Int
     while i <= len(array)-1:
         if j <= len(keyArray) - 1 :
             value = array[i] + keyArray[j]
@@ -46,15 +39,13 @@ def vigenere(array, keyword) :
         else :
             j = -1
             i -= 1
-
         i += 1
         j += 1
-
     return(new_Array)
 
+#RSA encoding use n and e to create a message
 def rsa(array, n, e):
     res = []
-
     for a in array:
         temp_e = e
         encoded_car = 1
@@ -67,6 +58,8 @@ def rsa(array, n, e):
         res.append(encoded_car)
     return res
 
+
+#Key generation RSA
 def generate_rsa_keys():
     prime_numbers = [int(np.random.choice(array_prime_numbers)), int(np.random.choice(array_prime_numbers))]
     p = prime_numbers[0]
@@ -85,6 +78,7 @@ def generate_rsa_keys():
     
     return [(n, e), (n, d)]
 
+#Calculate the e which should be coprime and smaller than k
 def generate_rsa_e(k):
     x = 0
     e = 0
@@ -92,7 +86,7 @@ def generate_rsa_e(k):
         e = random.randint(2, k-1)
         x = math.gcd(k, e)
     return e
-
+#Find d and b
 def extended_gcd(a, b):
     if b == 0: return 1, 0
   
@@ -113,45 +107,46 @@ def modPow(b, m, e):
         e = e >> 1
     return r
 
-def diffieHellman(a, array) :
+#Diffie Hellman encoding
+def generatediffieHellmanKeys(a,b) :
     p = int(np.random.choice(array_prime_numbers5000))
-
-    #GENERATOR
     n = (int(np.random.choice(array_prime_numbers)))
-    # find prime factors (n-1)
-    arrayOfPrimeFact = find_prime_factors(n-1)
-    # look if g is not congruent
-    g = int(random.randrange(1, n))
-    while(checkIfGood(g,arrayOfPrimeFact,n) == False) :
-        g = int(random.randrange(1, n))
+    g = generator(n)
+    ##Creation of the keys
+    sendA = g**a % p
+    sendB = g**b % p
+    sharedSecret = g**(a*b) % p
+    print(f"Shared secret is {sharedSecret}")
 
-
-    return(math.pow(g,a) % p)
-
-
-
-def checkIfGood(g, array,n) :
-    for i in range(array):
-        a = (n-1)/array_prime_numbers(i)
-        if math.pow(g,a) % n == 1 :
+#Check if congruent or not (for generator)
+def notCongruent(g,primeNumbers,n) :
+    for i in range(primeNumbers):
+        a = (n-1)/primeNumbers(i)
+        if int(math.pow(g,a)) % n == 1 :
             return(False)
     return(True)
+
 def find_prime_factors(n):
     factors = []
     divisor = 2
     while divisor <= n:
         if n % divisor == 0:
-            if not n in factors :
-                factors.append(divisor)
+            factors.append(divisor)
             n = n / divisor
         else:
             divisor += 1
-    return factors
-
-
-
-
-
+    unique_factors = list(set(factors))
+    return unique_factors
+#Creation of the generator
+def generator(n):
+    arrayOfPrime = find_prime_factors(n-1)
+    # look if g is not congruent
+    g = random.randint(1,256**4)
+    while not notCongruent(g,arrayOfPrime,n):
+        g = random.randint(1, 256 ** 4)
+        notCongruent(g,arrayOfPrime,n)
+    else:
+      return(g)
 
 if __name__ == "__main__":
 
