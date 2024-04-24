b1f4f32eaca4a5fe40c6d12c6a0f6005b4631c2c
GDbateaux <yolan.savioz@hes-so.ch>
2024-04-24 17:16:09 +0200

    catch exceptions

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from ui.ui import Ui
 
 if __name__ == "__main__":
     import sys
+
+    def except_hook(cls,exception,traceback):
+        try:
+            sys.__excepthook__(cls,exception,traceback)
+        except:
+            pass
+    sys.excepthook = except_hook
     app = QApplication(sys.argv)
     window = Ui()
     window.setWindowTitle("Cryptography")
